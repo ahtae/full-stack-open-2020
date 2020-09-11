9b6ab4ff0b29dcf801650e2feacedd22f96d6e02
Kristy Li <kristyli20.0@gmail.com>
2020-09-11 09:48:24 -0400

    feat: Complete 1.2: course information, step2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -14,17 +14,19 @@ const Content = ({
   exercises3,
 }) => {
   return (
-    <React.Fragment>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </React.Fragment>
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
+  );
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
   );
 };
 

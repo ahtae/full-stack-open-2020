081fc671cf89cbf83b06dca06915ee2c7b6ebbfe
Kristy Li <kristyli20.0@gmail.com>
2020-09-11 12:32:23 -0400

    feat: Complete 1.7: unicafe step2 and 1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,11 +10,19 @@ const Button = ({ text, handleClick }) => {
 };
 
 const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const positive = all ? good / all : 0;
+  const average = all ? (good - bad) / all : 0;
+  const percentageOfPositive = positive * 100;
+
   return (
     <React.Fragment>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {`${percentageOfPositive} %`}</p>
     </React.Fragment>
   );
 };

bfca50dde52cbeb6b53211653985f3e54432e82f
Kristy Li <kristyli20.0@gmail.com>
2020-09-13 12:39:25 -0400

    feat: Complete 3.3: Phonebook backend step3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -27,14 +27,25 @@ const persons = [
 
 app.use(express.json());
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons);
-});
-
 app.get('/info', (req, res) => {
   res.write(`<p>Phonebook has info for ${persons.length} people</p>`);
   res.write(`${new Date()}`);
   res.end();
+});
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
 });
 
 app.listen(PORT, () => {

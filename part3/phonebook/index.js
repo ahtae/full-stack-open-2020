eabbd0badd50f3393f2b9282eba415d0bdbdc615
Kristy Li <kristyli20.0@gmail.com>
2020-09-13 13:26:18 -0400

    feat: Complete 3.8*: Phonebook backend step8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -37,7 +37,20 @@ const unknownEndpoint = (request, response) => {
 
 app.use(express.json());
 
-app.use(morgan('tiny'));
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+      JSON.stringify(req.body),
+    ].join(' ');
+  })
+);
 
 app.get('/info', (req, res) => {
   res.write(`<p>Phonebook has info for ${persons.length} people</p>`);

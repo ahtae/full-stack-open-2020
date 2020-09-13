5ed718b885de386aa4c68b893f038fe1738e1a3b
Kristy Li <kristyli20.0@gmail.com>
2020-09-13 00:49:06 -0400

    feat: Complete 2.19: Phonebook step11 and 2.20*: Phonebook step12

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,12 +4,15 @@ import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 import Filter from './components/Filter';
 import personService from './services/persons';
+import Notification from './components/Notification';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newPhoneNumber, setNewPhoneNumber] = useState('');
   const [search, setSearch] = useState('');
+  const [message, setMessage] = useState(null);
+  const [messageType, setMessageType] = useState(null);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -31,18 +34,54 @@ const App = () => {
         )[0];
         const { id } = personToUpdate;
 
-        personService.update(id, personObject).then((updatedPerson) => {
-          const updatedPersons = persons.map((person) =>
-            person.name === newName ? updatedPerson : person
-          );
+        personService
+          .update(id, personObject)
+          .then((updatedPerson) => {
+            const updatedPersons = persons.map((person) =>
+              person.name === newName ? updatedPerson : person
+            );
 
-          setPersons(updatedPersons);
-        });
+            setPersons(updatedPersons);
+            setMessage(`Updated ${newName}`);
+            setMessageType('success');
+
+            setTimeout(() => {
+              setMessage(null);
+              setMessageType(null);
+            }, 5000);
+          })
+          .catch((error) => {
+            setMessage(`Person '${newName}' has already removed from server`);
+            setMessageType('error');
+
+            setTimeout(() => {
+              setMessage(null);
+              setMessageType(null);
+            }, 5000);
+          });
       }
     } else {
       personService
         .create(personObject)
-        .then((returnedPerson) => setPersons(persons.concat(returnedPerson)));
+        .then((returnedPerson) => {
+          setPersons(persons.concat(returnedPerson));
+          setMessage(`Added ${newName}`);
+          setMessageType('success');
+
+          setTimeout(() => {
+            setMessage(null);
+            setMessageType(null);
+          }, 5000);
+        })
+        .catch((error) => {
+          setMessage(`Person '${newName}' cannot be added to the server`);
+          setMessageType('error');
+
+          setTimeout(() => {
+            setMessage(null);
+            setMessageType(null);
+          }, 5000);
+        });
     }
 
     setNewName('');
@@ -56,9 +95,27 @@ const App = () => {
     if (window.confirm(`Delete ${name}?`)) {
       personService
         .remove(id)
-        .then((removedPerson) =>
-          setPersons(persons.filter((person) => person.id !== id))
-        );
+        .then((removedPerson) => {
+          setPersons(persons.filter((person) => person.id !== id));
+          setMessage(`Removed ${name}`);
+          setMessageType('success');
+
+          setTimeout(() => {
+            setMessage(null);
+            setMessageType(null);
+          }, 5000);
+        })
+        .catch((error) => {
+          setMessage(
+            `Information of ${person.name} has already been removed from the server`
+          );
+          setMessageType('error');
+
+          setTimeout(() => {
+            setMessage(null);
+            setMessageType(null);
+          }, 5000);
+        });
     }
   };
 
@@ -75,13 +132,25 @@ const App = () => {
   };
 
   const hook = () => {
-    personService.getAll().then((initialPersons) => setPersons(initialPersons));
+    personService
+      .getAll()
+      .then((initialPersons) => setPersons(initialPersons))
+      .catch((error) => {
+        setMessage('Cannot retrieve all the persons in the phonebook');
+        setMessageType('error');
+
+        setTimeout(() => {
+          setMessage(null);
+          setMessageType(null);
+        }, 5000);
+      });
   };
 
   useEffect(hook, []);
 
   return (
     <div>
+      <Notification message={message} messageType={messageType} />
       <Header title="Phonebook" />
       <Filter search={search} handleSearchChange={handleSearchChange} />
       <PersonForm

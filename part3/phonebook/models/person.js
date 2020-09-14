dcea78a17b4c0e15f25b0d4b6c519cad215b3e20
Kristy Li <kristyli20.0@gmail.com>
2020-09-13 22:53:11 -0400

    feat: Complete 3.22: Lint configuration

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -1,43 +1,45 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-const url = process.env.MONGODB_URI;
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+const url = process.env.MONGODB_URI
 
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
 
-console.log('connecting to', url);
+console.log('connecting to', url)
 
 mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
-    console.log('connected to MongoDB');
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true, minlength: 3 },
   number: { type: String, required: true, minlength: 8 },
-});
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
+
+personSchema.plugin(uniqueValidator)
 
 mongoose.plugin((schema) => {
-  schema.pre('findOneAndUpdate', setRunValidators);
-  schema.pre('updateMany', setRunValidators);
-  schema.pre('updateOne', setRunValidators);
-  schema.pre('update', setRunValidators);
-});
+  schema.pre('findOneAndUpdate', setRunValidators)
+  schema.pre('updateMany', setRunValidators)
+  schema.pre('updateOne', setRunValidators)
+  schema.pre('update', setRunValidators)
+})
 
 function setRunValidators() {
-  this.setOptions({ runValidators: true });
+  this.setOptions({ runValidators: true })
 }
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)

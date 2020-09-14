4a314df8eaf691937e1cffe5d2ad15cd4668cff2
Kristy Li <kristyli20.0@gmail.com>
2020-09-14 12:38:15 -0400

    feat: Complete 4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,9 +4,19 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
-}
+};
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((blogWithMostLikes, blog) => {
+    return Object.keys(blogWithMostLikes).length === 0 ||
+      blog.likes > blogWithMostLikes.likes
+      ? { title: blog.title, author: blog.author, likes: blog.likes }
+      : blogWithMostLikes;
+  }, {});
+};
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 };

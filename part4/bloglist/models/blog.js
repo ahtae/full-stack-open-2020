1d9206fe3ae69819acf51ee13f393d7d21975510
Kristy Li <kristyli20.0@gmail.com>
2020-09-14 15:56:26 -0400

    feat: Complete 4.8: Blog list tests, step1

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
+  title: {
+    type: String,
+    required: true,
+    minLength: 3
+  },
+  author: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  url: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  likes: {
+    type: Number,
+    default: 0
+  }
 });
 
 const Blog = mongoose.model('Blog', blogSchema);

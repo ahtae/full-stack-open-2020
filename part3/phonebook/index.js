ee58bc6719c7ea8c53fa4f6d904037118c7660a6
Kristy Li <kristyli20.0@gmail.com>
2020-09-14 23:14:34 -0400

    fix: Bug with PUT route

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,34 +1,34 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const Person = require('./models/person')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const Person = require('./models/person');
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
-}
+  res.status(404).send({ error: 'unknown endpoint' });
+};
 
 const errorHandler = (error, req, res, next) => {
   if (error.name === 'CastError') {
-    res.status(400).send({ error: 'malformatted id' })
+    res.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
-    res.status(400).json({ error: error.message })
+    res.status(400).json({ error: error.message });
   } else if (error.name === 'MongoError') {
-    res.status(400).json({ error: 'Name already exists!' })
+    res.status(400).json({ error: 'Name already exists!' });
   }
 
-  next(error)
-}
+  next(error);
+};
 
-app.use(express.static('build'))
+app.use(express.static('build'));
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(
   morgan((tokens, req, res) => {
@@ -41,84 +41,85 @@ app.use(
       tokens['response-time'](req, res),
       'ms',
       tokens.method(req, res) === 'POST' ? JSON.stringify(req.body) : null,
-    ].join(' ')
+    ].join(' ');
   })
-)
+);
 
 app.get('/info', (req, res, next) => {
   Person.find({})
     .then((persons) => {
-      res.write(`<p>Phonebook has info for ${persons.length} people</p>`)
-      res.write(`${new Date()}`)
-      res.end()
+      res.write(`<p>Phonebook has info for ${persons.length} people</p>`);
+      res.write(`${new Date()}`);
+      res.end();
     })
-    .catch((error) => next(error))
-})
+    .catch((error) => next(error));
+});
 
 app.get('/api/persons', (req, res, next) => {
   Person.find({})
     .then((persons) => res.json(persons))
-    .catch((error) => next(error))
-})
+    .catch((error) => next(error));
+});
 
 app.get('/api/persons/:id', (req, res, next) => {
-  const { id } = req.params
+  const { id } = req.params;
 
   Person.findById(id)
     .then((person) => {
       if (person) {
-        res.json(person)
+        res.json(person);
       } else {
-        res.status(404).end()
+        res.status(404).end();
       }
     })
-    .catch((error) => next(error))
-})
+    .catch((error) => next(error));
+});
 
 app.delete('/api/persons/:id', (req, res, next) => {
-  const { id } = req.params
+  const { id } = req.params;
 
   Person.findByIdAndRemove(id)
     .then((result) => {
-      res.status(204).end()
+      res.status(204).end();
     })
-    .catch((error) => next(error))
-})
+    .catch((error) => next(error));
+});
 
 app.post('/api/persons', (req, res, next) => {
-  const { body } = req
+  const { body } = req;
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  })
+  });
 
   person
     .save()
     .then((savedPerson) => savedPerson.toJSON())
     .then((savedAndFormattedPerson) => res.json(savedAndFormattedPerson))
-    .catch((error) => next(error))
-})
+    .catch((error) => next(error));
+});
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const { id } = req.params
-  const { body } = req
+  const { id } = req.params;
+  const { body } = req;
 
   const informationToUpdate = {
+    name: body.name,
     number: body.number,
-  }
+  };
 
-  Person.findByIdAndUpdate(id, informationToUpdate)
+  Person.findByIdAndUpdate(id, informationToUpdate, { new: true })
     .then((updatedPerson) => {
-      res.json(updatedPerson)
+      res.json(updatedPerson);
     })
-    .catch((error) => next(error))
-})
+    .catch((error) => next(error));
+});
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});

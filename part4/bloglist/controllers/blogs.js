661402a798d5dd55636055e7951b3ccddf59964e
Kristy Li <kristyli20.0@gmail.com>
2020-09-15 13:26:37 -0400

    feat: Complete 4.19: bloglist expansion, step7 and 4.20*: bloglist expansion, step8

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,16 +1,23 @@
+const config = require('../utils/config');
+const jwt = require('jsonwebtoken');
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
+  console.log(request);
 
   response.json(blogs.map((blog) => blog.toJSON()));
 });
 
 blogsRouter.post('/', async (request, response) => {
   const { body } = request;
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(request.token, config.SECRET);
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
   const blog = new Blog({
     title: body.title,
     author: body.author,
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,32 +1,48 @@
-const logger = require('./logger')
+const logger = require('./logger');
 
 const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method)
-  logger.info('Path:  ', request.path)
-  logger.info('Body:  ', request.body)
-  logger.info('---')
-  next()
-}
+  logger.info('Method:', request.method);
+  logger.info('Path:  ', request.path);
+  logger.info('Body:  ', request.body);
+  logger.info('---');
+  next();
+};
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
-const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization');
 
-  if (error.name === 'CastError') {
-    response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    response.status(400).json({ error: error.message })
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7);
   }
 
-  next(error)
-}
+  next();
+};
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message);
+
+  if (error.name === 'CastError') {
+    response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    response.status(400).json({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token',
+    });
+  }
+
+  logger.error(error.message);
+
+  next(error);
+};
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
-}
-
+  errorHandler,
+  tokenExtractor,
+};

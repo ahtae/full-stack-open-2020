af1c7c8c1eb616e488cef00b2693df9ee975887f
Kristy Li <kristyli20.0@gmail.com>
2020-09-15 09:24:16 -0400

    feat: Complete 4.15: bloglist expansion, step3

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 
 logger.info('connecting to', config.MONGODB_URI);
 
@@ -26,6 +27,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
 app.use(middleware.unknownEndpoint);

14a58ceee10a93bee4da214f11eeceb105d4ddb7
Kristy Li <kristyli20.0@gmail.com>
2020-09-20 19:37:55 -0400

    feat: Complete 5.16*: Blog list tests, step4

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
+import BlogForm from './BlogForm';
 
 test('renders content', () => {
   const blog = {
@@ -87,4 +88,36 @@ test('clicking the button twice calls event handler twice', () => {
   fireEvent.click(likeButton);
 
   expect(mockHandler.mock.calls).toHaveLength(2);
+});
+
+test('<BlogForm /> updates parent state and calls onSubmit', () => {
+  const createBlog = jest.fn();
+
+  const component = render(<BlogForm createBlog={createBlog} />);
+
+  const titleInput = component.container.querySelector('#title');
+  const authorInput = component.container.querySelector('#author');
+  const urlInput = component.container.querySelector('#url');
+  const form = component.container.querySelector('form');
+
+  fireEvent.change(titleInput, {
+    target: { value: 'testing of forms could be easier' },
+  });
+
+  fireEvent.change(authorInput, {
+    target: { value: 'bojack horseman' },
+  });
+
+  fireEvent.change(urlInput, {
+    target: { value: 'www.testing.com' },
+  });
+
+  fireEvent.submit(form);
+
+  expect(createBlog.mock.calls).toHaveLength(1);
+  expect(createBlog.mock.calls[0][0].title).toBe(
+    'testing of forms could be easier'
+  );
+  expect(createBlog.mock.calls[0][0].author).toBe('bojack horseman');
+  expect(createBlog.mock.calls[0][0].url).toBe('www.testing.com');
 });

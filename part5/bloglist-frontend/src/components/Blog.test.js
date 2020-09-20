5646d9d73d999890fc3ddfc334cceeda13f831e7
Kristy Li <kristyli20.0@gmail.com>
2020-09-20 18:22:32 -0400

    feat: Complete 5.15: Blog list tests, step3

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -14,11 +14,8 @@ test('renders content', () => {
   const component = render(<Blog blog={blog} />);
 
   expect(component.container).toHaveTextContent('this is a rendered blog');
-
   expect(component.container).toHaveTextContent('me');
-
   expect(component.container).not.toHaveTextContent(100);
-
   expect(component.container).not.toHaveTextContent('www.nourl.com');
 });
 
@@ -43,26 +40,51 @@ test('button can be used to view and hide blog', () => {
   );
 
   const component = render(<Blog blog={blog} />);
-
   const button = component.getByText('view');
   fireEvent.click(button);
 
   expect(component.container).toHaveTextContent('this is a rendered blog');
-
   expect(component.container).toHaveTextContent('me');
-
   expect(component.container).toHaveTextContent(100);
-
   expect(component.container).toHaveTextContent('www.nourl.com');
 
   const closeButton = component.getByText('hide');
   fireEvent.click(closeButton);
 
   expect(component.container).toHaveTextContent('this is a rendered blog');
-
   expect(component.container).toHaveTextContent('me');
-
   expect(component.container).not.toHaveTextContent(100);
-
   expect(component.container).not.toHaveTextContent('www.nourl.com');
+});
+
+test('clicking the button twice calls event handler twice', () => {
+  const blog = {
+    title: 'this is a rendered blog',
+    author: 'me',
+    likes: 100,
+    url: 'www.nourl.com',
+    user: {
+      name: 'bob',
+    },
+  };
+
+  window.localStorage.setItem(
+    'loggedBlogappUser',
+    JSON.stringify({
+      user: {
+        name: 'bob',
+      },
+    })
+  );
+
+  const mockHandler = jest.fn();
+  const component = render(<Blog blog={blog} upvoteBlog={mockHandler} />);
+  const button = component.getByText('view');
+  fireEvent.click(button);
+
+  const likeButton = component.getByText('like');
+  fireEvent.click(likeButton);
+  fireEvent.click(likeButton);
+
+  expect(mockHandler.mock.calls).toHaveLength(2);
 });

0ec474424369c0a3187e890a20ef5323783a3e0e
Kristy Li <kristyli20.0@gmail.com>
2020-09-22 09:17:25 -0400

    feat: Complete 5.19: bloglist end to end testing, step3

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -20,6 +20,7 @@ describe('Blog app', function () {
 
   describe('Login', function () {
     it('succeeds with correct credentials', function () {
+      cy.contains('login').click()
       cy.get('#username').type('eric')
       cy.get('#password').type('eric')
       cy.get('#login-button').click()
@@ -37,4 +38,27 @@ describe('Blog app', function () {
       cy.get('html').should('not.contain', 'eric andre logged in')
     });
   });
+
+  describe.only('When logged in', function() {
+    beforeEach(function() {
+      cy.login({ username: 'eric', password: 'eric' })
+      cy.visit('http://localhost:3000');
+    })
+
+    it('A blog can be created and is added to the list of all blogs', function() {
+      cy.contains('create new blog').click()
+      cy.get('#title').type('a blog created by cypress')
+      cy.get('#author').type('sherlock')
+      cy.get('#url').type('www.sholmes.com')
+      cy.get('#create-button').click()
+      cy.get('.success').should('contain', 'a blog created by cypress').and('have.css', 'color', 'rgb(0, 128, 0)').and('have.css', 'border-style', 'solid')
+
+      cy.contains('create new blog').click()
+      cy.get('#title').type('a blog created by cypress once again')
+      cy.get('#author').type('ash')
+      cy.get('#url').type('www.ashk.com')
+      cy.get('#create-button').click()
+      cy.get('.success').should('contain', 'a blog created by cypress').and('have.css', 'color', 'rgb(0, 128, 0)').and('have.css', 'border-style', 'solid')
+    })
+  })
 });
--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -40,7 +40,7 @@ const BlogForm = ({ createBlog }) => {
         url: <input id="url" value={newUrl} onChange={handleUrlChange} />
       </div>
 
-      <button type="submit">create</button>
+      <button id="create-button" type="submit">create</button>
     </form>
   );
 };

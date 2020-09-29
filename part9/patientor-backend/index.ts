e81df4d4940ace9e1d949ad1877d1158b3f5a274
Kristy Li <kristyli20.0@gmail.com>
2020-09-29 15:25:15 -0400

    feat: Complete 9.9: Patientor backend, step2

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
-const PORT = 3000;
+const PORT = 3001;
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
